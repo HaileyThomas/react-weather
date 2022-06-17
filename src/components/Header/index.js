9c3ac043f9464a0861eaa397a35efef89b250a7e
HaileyThomas <haileyraebeauty@gmail.com>
2022-06-17 15:57:21 -0700

    set up basic header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useParams, Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
+
+const Header = () => {
+  return (
+    <div className="header-component">
+      <div className="header-right">
+        <h1 className="main-header">Weather Dashboard</h1>
+        <Icon icon="line-md:menu" className="menu-icon" />
+      </div>
+      <div className="header-left">
+        <Icon icon="charm:sun" className="light-icon" />
+        <Icon icon="charm:moon" className="dark-icon" />
+        <Icon icon="dashicons:info-outline" className="about-icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
