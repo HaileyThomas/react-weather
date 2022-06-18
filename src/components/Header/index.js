58e685dfc7357e37800f6efcee820b28c2704426
HaileyThomas <haileyraebeauty@gmail.com>
2022-06-18 14:22:16 -0700

    work on styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,15 +3,15 @@ import { Icon } from "@iconify/react";
 
 const Header = () => {
   return (
-    <div className="header-component">
-      <div className="header-right">
+    <div className="header-container">
+      <div className="header-left">
         <h1 className="main-header">Weather Dashboard</h1>
         <Icon icon="line-md:menu" className="menu-icon" />
       </div>
-      <div className="header-left">
+      <div className="header-right">
         <Icon icon="charm:sun" className="light-icon" />
         <Icon icon="charm:moon" className="dark-icon" />
-        <Icon icon="dashicons:info-outline" className="about-icon" />
+        <Icon icon="dashicons:info-outline" className="about-icon col-1" />
       </div>
     </div>
   );
