da5886aeb6772117dca352d22cd30d2ac809891b
HaileyThomas <haileyraebeauty@gmail.com>
2022-06-20 17:30:26 -0700

    console log images and data, work on getting data into variable

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -5,8 +5,14 @@ import Forecast from "../Forecast";
 import Details from "../Details";
 
 const Results = ({ city, setCity }) => {
-  const [cityData, setCityData] = useState([]);
-  var weatherData;
+  const [weatherData, setWeatherData] = useState([]);
+  var cityInfo;
+  var cityData;
+  var cityImageWeb;
+  var cityImageMobile;
+
+  var str = city;
+  str = str.replace(/\s+/g, "-").toLowerCase();
 
   const cityLocationUrl =
     "http://api.openweathermap.org/geo/1.0/direct?q=" +
@@ -14,8 +20,15 @@ const Results = ({ city, setCity }) => {
     "&limit=1&appid=" +
     "6ff484b66ed5b4a802761c069566a64c";
 
+  const cityImageUrl =
+    "https://api.teleport.org/api/urban_areas/slug:" + str + "/images/";
+
   const getCityLocation = () => {
     return fetch(cityLocationUrl).then((response) => response.json());
+  };
+
+  const getCityImage = () => {
+    return fetch(cityImageUrl).then((response) => response.json());
   };
 
   const getWeather = (weatherApiUrl) => {
@@ -26,19 +39,41 @@ const Results = ({ city, setCity }) => {
     getCityLocation().then((data) => {
       const latitude = data[0].lat;
       const longitude = data[0].lon;
-      const weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=" + "hourly,minutely" + "&units=" + "imperial" + "&appid=" + "d50140606331b5f0875df8b66c236b78";
-      getWeather(weatherApiUrl).then((data) => {
-        weatherData = data;
-        console.log(weatherData);
-      })
+      const weatherApiUrl =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        latitude +
+        "&lon=" +
+        longitude +
+        "&exclude=" +
+        "hourly,minutely" +
+        "&units=" +
+        "imperial" +
+        "&appid=" +
+        "d50140606331b5f0875df8b66c236b78";
+      fetch(weatherApiUrl)
+        // get and parse response
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          setWeatherData(data);
+          console.log(weatherData);
+        });
+    });
+    getCityImage().then((data) => {
+      cityImageWeb = data.photos[0].image.web;
+      cityImageMobile = data.photos[0].image.mobile;
+      console.log("web image:" + cityImageWeb);
+      console.log("mobile image:" + cityImageMobile);
     });
   }, []);
 
   return (
     <div className="results-container">
-      <Main />
-      <Forecast />
-      <Details />
+      <Main weatherData={weatherData} />
+      <Forecast weatherData={weatherData} />
+      <Details weatherData={weatherData} />
     </div>
   );
 };
