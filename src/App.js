54eaf7bf5d7feb836698ddefce0d3995ba2ad5e6
HaileyThomas <haileyraebeauty@gmail.com>
2022-06-21 15:43:18 -0700

    show current data and have catch error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import useLocalStorage from "use-local-storage";
 
 import Header from "./components/Header";
@@ -8,28 +8,57 @@ import Search from "./components/Search";
 import Results from "./components/Results";
 
 function App() {
-  const [city, setCity] = useState("New York");
-  const [cityImgWide, setCityImgWide] = useState("");
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+  const weatherApiUrl =
+    "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+    lat +
+    "&lon=" +
+    long +
+    "&exclude=" +
+    "minutely" +
+    "&units=" +
+    "imperial" +
+    "&appid=" +
+    "d50140606331b5f0875df8b66c236b78";
+
   const defaultDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
   const [theme, setTheme] = useLocalStorage(
     "theme",
     defaultDark ? "dark" : "light"
   );
 
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+      await fetch(weatherApiUrl)
+        .then((res) => res.json())
+        .then((result) => {
+          setData(result);
+          console.log(result);
+        });
+    };
+    fetchData();
+  }, [lat, long]);
+
   return (
     <div className="App" data-theme={theme}>
       <Header theme={theme} setTheme={setTheme} />
       <div className="app-container">
         <div className="side-container">
-          <Saved city={city} setCity={setCity} cityImgWide={cityImgWide} />
+          <Saved />
         </div>
         <div className="main-container">
-          <Search city={city} setCity={setCity} />
-          <Results
-            city={city}
-            cityImgWide={cityImgWide}
-            setCityImgWide={setCityImgWide}
-          />
+          <Search />
+          {typeof data.current != "undefined" ? (
+            <Results weatherData={data} />
+          ) : (
+            <div></div>
+          )}
         </div>
       </div>
       <Footer />
--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -1,74 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Icon } from "@iconify/react";
 
-const Results = ({ city, cityImgWide, setCityImgWide }) => {
-  const [newData, setNewData] = useState({});
-  var weatherData;
-  var cityImage;
-
-  var str = city;
-  str = str.replace(/\s+/g, "-").toLowerCase();
-
-  var cityName = city
-    .toLowerCase()
-    .split(" ")
-    .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
-    .join(" ");
-
-  const cityLocationUrl =
-    "http://api.openweathermap.org/geo/1.0/direct?q=" +
-    city +
-    "&limit=1&appid=" +
-    "6ff484b66ed5b4a802761c069566a64c";
-
-  const cityImageUrl =
-    "https://api.teleport.org/api/urban_areas/slug:" + str + "/images/";
-
-  const getCityLocation = () => {
-    return fetch(cityLocationUrl).then((response) => response.json());
-  };
-
-  const getCityImage = () => {
-    return fetch(cityImageUrl).then((response) => response.json());
-  };
-
-  const getWeather = (weatherApiUrl) => {
-    await fetch(weatherApiUrl).then((response) => response.json());
-  };
-
-  useEffect(() => {
-    getCityLocation().then((data) => {
-      const latitude = data[0].lat;
-      const longitude = data[0].lon;
-      const weatherApiUrl =
-        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
-        latitude +
-        "&lon=" +
-        longitude +
-        "&exclude=" +
-        "minutely" +
-        "&units=" +
-        "imperial" +
-        "&appid=" +
-        "d50140606331b5f0875df8b66c236b78";
-      getWeather(weatherApiUrl).then((data) => {
-        weatherData = data;
-        console.log(weatherData);
-        setNewData(data);
-      });
-    });
-    getCityImage().then((data) => {
-      setCityImgWide(data.photos[0].image.web);
-      cityImage = data.photos[0].image.mobile;
-    });
-  }, []);
-
+const Results = ({ weatherData }) => {
   return (
     <div className="results-container">
       <div className="main-results-container">
         <div className="main-header-container">
           <div className="main-header-left">
-            <h2>{cityName}</h2>
+            <h2>name here</h2>
           </div>
           <div className="main-header-right">
             <Icon icon="akar-icons:star" className="icon default-icon" />
@@ -77,17 +16,21 @@ const Results = ({ city, cityImgWide, setCityImgWide }) => {
         </div>
         <div className="main-content-container">
           <div className="main-content-left">
-            <img src={require("../../assets/icons/" + newData.current.weather[0].icon + ".png")} />
-            <h1>{newData.current.temp} °F</h1>
-            <p>{newData.current.weather[0].description}</p>
+            <img
+              src={require("../../assets/icons/" +
+                weatherData.current.weather[0].icon +
+                ".png")}
+            />
+            <h1>{weatherData.current.temp} °F</h1>
+            <p>{weatherData.current.weather[0].description}</p>
           </div>
           <div className="main-content-right">
-            <p>Feels Like: {newData.current.feels_like} °F</p>
-            <p>Humidity: {newData.current.humidity}</p>
-            <p>Clouds: {newData.current.clouds}</p>
-            <p>Dew Point: {newData.current.dew_point}</p>
-            <p>Wind Speed: {newData.current.wind_speed}</p>
-            <p>UVI: {newData.current.uvi}</p>
+            <p>Feels Like: {weatherData.current.feels_like} °F</p>
+            <p>Humidity: {weatherData.current.humidity} %</p>
+            <p>Clouds: {weatherData.current.clouds}</p>
+            <p>Dew Point: {weatherData.current.dew_point}</p>
+            <p>Wind Speed: {weatherData.current.wind_speed} MPH</p>
+            <p>UVI: {weatherData.current.uvi}</p>
           </div>
         </div>
       </div>
